8950756054501d3f1667ce9c2cd60bce774ac9ef
Yash Soni <yash.soni2737@gmail.com>
2023-01-21 12:12:40 +0000

    dallE enablement

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+  wallE_API_KEY: process.env.NODE_ENV === 'production' ? process.env.OPENAI_API_KEY : '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'
 };
