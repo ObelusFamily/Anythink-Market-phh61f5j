6c731f5bdd65039fc8b7cb1a1835dd46fd2ec3b2
Yash Soni <yash.soni2737@gmail.com>
2023-01-22 14:45:42 +0000

    removed api key from codebase

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
-  wallE_API_KEY: process.env.NODE_ENV === 'production' ? process.env.OPENAI_API_KEY : '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'
+  wallE_API_KEY: process.env.OPENAI_API_KEY
 };
